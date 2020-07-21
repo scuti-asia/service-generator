1d2c095e10c27c28be88bb8939644149a17d43b3
zmap95 <canhht@scuti.asia>
2020-07-21 11:36:12 +0700

    Update composer & boot command

--- src/ServiceGeneratorProvider.php
+++ src/ServiceGeneratorProvider.php
@@ -3,6 +3,7 @@
 namespace Scuti\Admin\ServiceGenerator;
 
 use Illuminate\Support\ServiceProvider;
+use Scuti\Admin\ServiceGenerator\Commands\CreateNewService;
 
 class ServiceGeneratorProvider extends ServiceProvider
 {
@@ -26,5 +27,11 @@ class ServiceGeneratorProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/commands' => base_path('app/Console/Commands/')
         ], "service-generator");
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                CreateNewService::class,
+            ]);
+        }
     }
 }
--- src/commands/CreateNewService.php
+++ src/commands/CreateNewService.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 
-class ServiceGenerator extends Command
+class CreateNewService extends Command
 {
     /**
      * The name and signature of the console command.
